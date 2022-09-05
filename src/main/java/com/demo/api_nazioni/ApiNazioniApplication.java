29936562989e460b613aa65cebc801c057d6f3bc
Stefano Scarpetta <scarpettastefano.main@gmail.com>
2022-09-05 10:07:03 +0200

    Initial commit

---
+++ src/main/java/com/demo/api_nazioni/ApiNazioniApplication.java
@@ -0,0 +1,13 @@
+package com.demo.api_nazioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNazioniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNazioniApplication.class, args);
+	}
+
+}

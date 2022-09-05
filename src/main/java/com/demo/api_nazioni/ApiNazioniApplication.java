defd0da0bda2c1bafdc8ce39c5db193d03e31372
Stefano Scarpetta <scarpettastefano.main@gmail.com>
2022-09-05 14:54:32 +0200

    Risoluzione problemi e host API funzionante
    Risoluzione bug url e programma funzionante hostato su localhost:8080

--- src/main/java/com/demo/api_nazioni/ApiNazioniApplication.java
+++ src/main/java/com/demo/api_nazioni/ApiNazioniApplication.java
@@ -3,25 +3,26 @@ package com.demo.api_nazioni;
 import com.demo.api_nazioni.RestApi.ToRestApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import java.io.IOException;
 import java.text.ParseException;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ApiNazioniApplication {
 
     public static void main(String[] args) throws IOException, ParseException, org.json.simple.parser.ParseException {
         SpringApplication.run(ApiNazioniApplication.class, args);
 
-        String link = "https://webgate.ec.europa.eu/comp/redisstat/api/dissemination/sdmx/2.1/data/comp_ag_x$comp_ag_01?format=json&compressed=false";
-        String directory = "C:\\Users\\stefa\\Documents\\GitHub\\ProgettoBackendJava\\Risorse\\Scaricato.json";
-        String api = "C:\\Users\\stefa\\Documents\\GitHub\\ProgettoBackendJava\\RestApi\\db.json";
+        //String link = "https://webgate.ec.europa.eu/comp/redisstat/api/dissemination/sdmx/2.1/data/comp_ag_x$comp_ag_01?format=json&compressed=false";
+        //String directory = "C:\\Users\\stefa\\Documents\\GitHub\\ProgettoBackendJava\\Risorse\\Scaricato.json";
+        //String api = "C:\\Users\\stefa\\Documents\\GitHub\\ProgettoBackendJava\\RestApi\\db.json";
         //File out = new File(directory);
         //Download download = new Download(link, out); //il file da scaricare dal link non esiste più, la parte di programma riguardante il download funziona
         //download.run();
 
-        ToRestApi toRestApi = new ToRestApi(directory, api);
-        toRestApi.run();
+        //ToRestApi toRestApi = new ToRestApi(directory, api);
+        //toRestApi.run();
     }
 
 }
---
+++ src/main/java/com/demo/api_nazioni/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.demo.api_nazioni.controller;
+
+import com.demo.api_nazioni.RestApi.ToRestApi;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+@Controller //identifica questa classe come un controller
+public class HomeController {
+    @RequestMapping("/") //il metodo action risponde ad un determinato path specificato come parametro /
+    @ResponseBody //il contenuto del metodo sopra comparirà nella pagina mostrata all'utente
+    public JSONObject init() throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader("C:\\Users\\stefa\\Desktop\\Java\\API-nazioni\\API nazioni\\src\\main\\java\\com\\demo\\api_nazioni\\RestApi\\db.json"));
+        JSONObject file = (JSONObject) obj;
+        return file;
+    }
+}
